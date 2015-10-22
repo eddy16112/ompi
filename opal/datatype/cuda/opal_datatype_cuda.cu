35254196116af250b76a81bdb30040e0dfd181de
eddy16112 <eddy16112@gmail.com>
2015-10-22 17:36:31 -0400

    offset instead of actual addess, and lots of clean up for unused functions
    Conflicts:
    	opal/datatype/cuda/opal_datatype_cuda.cu
    	opal/datatype/cuda/opal_datatype_cuda_internal.cuh
    	opal/datatype/cuda/opal_datatype_pack_cuda_kernel.cu
    	opal/datatype/cuda/opal_datatype_pack_cuda_wrapper.cu
    	opal/datatype/cuda/opal_datatype_unpack_cuda_kernel.cu
    	opal/datatype/cuda/opal_datatype_unpack_cuda_wrapper.cu
    	opal/datatype/opal_datatype_gpu.c

--- opal/datatype/cuda/opal_datatype_cuda.cu
+++ opal/datatype/cuda/opal_datatype_cuda.cu
@@ -9,56 +9,14 @@
 #include <assert.h>
 #include <stdarg.h> 
 
-/*
- * NOTE: The order of this array *MUST* match what is listed in datatype.h
- * (use of designated initializers should relax this restrictions some)
- */
-/*
-OPAL_DECLSPEC const size_t opal_datatype_basicDatatypesSize[OPAL_DATATYPE_MAX_PREDEFINED] = {
-    OPAL_DATATYPE_LOOP_SIZE,
-    OPAL_DATATYPE_END_LOOP_SIZE,
-    OPAL_DATATYPE_LB_SIZE,
-    OPAL_DATATYPE_UB_SIZE,
-    OPAL_DATATYPE_INT1_SIZE,
-    OPAL_DATATYPE_INT2_SIZE,
-    OPAL_DATATYPE_INT4_SIZE,
-    OPAL_DATATYPE_INT8_SIZE,
-    OPAL_DATATYPE_INT16_SIZE,   
-    OPAL_DATATYPE_UINT1_SIZE,
-    OPAL_DATATYPE_UINT2_SIZE,
-    OPAL_DATATYPE_UINT4_SIZE,
-    OPAL_DATATYPE_UINT8_SIZE,
-    OPAL_DATATYPE_UINT16_SIZE,  
-    OPAL_DATATYPE_FLOAT2_SIZE,
-    OPAL_DATATYPE_FLOAT4_SIZE,
-    OPAL_DATATYPE_FLOAT8_SIZE,
-    OPAL_DATATYPE_FLOAT12_SIZE,
-    OPAL_DATATYPE_FLOAT16_SIZE,
-    OPAL_DATATYPE_FLOAT_COMPLEX_SIZE,
-    OPAL_DATATYPE_DOUBLE_COMPLEX_SIZE,
-    OPAL_DATATYPE_LONG_DOUBLE_COMPLEX_SIZE,
-    OPAL_DATATYPE_BOOL_SIZE,
-    OPAL_DATATYPE_WCHAR_SIZE,
-    OPAL_DATATYPE_UNAVAILABLE_SIZE,
-};
-*/
-/***** my variables ********/
-
 
 ddt_cuda_list_t *cuda_free_list;
 ddt_cuda_device_t *cuda_device;
-ddt_cuda_desc_t *cuda_desc_d, *cuda_desc_h;
-unsigned char *pBaseBuf_GPU, *gpu_src_const, *gpu_dest_const;
-unsigned char *ddt_cuda_pack_buffer, *ddt_cuda_unpack_buffer;
 ddt_cuda_stream_t* cuda_streams;
 struct iovec cuda_iov[CUDA_NB_IOV];
 uint32_t cuda_iov_count;
-ddt_cuda_description_dist_t description_dist_h[CUDA_MAX_NB_BLOCKS];
-ddt_cuda_description_dist_t* description_dist_d;
-ddt_cuda_iov_dist_t cuda_iov_dist_h[NB_STREAMS][CUDA_MAX_NB_BLOCKS];
+ddt_cuda_iov_dist_t* cuda_iov_dist_h[NB_STREAMS];
 ddt_cuda_iov_dist_t* cuda_iov_dist_d[NB_STREAMS];
-dt_elem_desc_t* description_d;
-uint8_t opal_datatype_cuda_debug;
 
 //uint8_t ALIGNMENT_DOUBLE, ALIGNMENT_FLOAT, ALIGNMENT_CHAR;
 
@@ -202,6 +160,17 @@ static inline void cuda_list_item_merge_by_addr(ddt_cuda_list_t *list)
     }
 }
 
+void opal_cuda_output(int output_id, const char *format, ...)
+{
+    if (output_id >= 0 && output_id <= OPAL_DATATYPE_CUDA_DEBUG_LEVEL) {
+        va_list arglist;
+        fprintf( stderr, "[Debug %d]: ", output_id );
+        va_start(arglist, format);
+        vfprintf(stderr, format, arglist);
+        va_end(arglist);
+    }
+}
+
 void opal_datatype_cuda_init(void)
 {
     uint32_t i;
@@ -213,7 +182,6 @@ void opal_datatype_cuda_init(void)
         opal_cuda_output(0, "Cannot retrieve the device being used. Drop CUDA support!\n");
         return;
     }    
-    printf("current device %d\n", device);
 
     cuda_free_list = init_cuda_free_list();
     
@@ -224,6 +192,7 @@ void opal_datatype_cuda_init(void)
         if (cudaMalloc((void **)(&gpu_ptr), sizeof(char)*DT_CUDA_BUFFER_SIZE) != cudaSuccess) {
             DT_CUDA_DEBUG( opal_cuda_output( 0, "cudaMalloc is failed in GPU %d\n", i); );
         }
+        DT_CUDA_DEBUG ( opal_cuda_output(2, "DDT engine cudaMalloc buffer %p in GPU %d\n", gpu_ptr, i););
         cudaMemset(gpu_ptr, 0, sizeof(char)*DT_CUDA_BUFFER_SIZE);
         cuda_device[i].gpu_buffer = gpu_ptr;
         
@@ -241,33 +210,6 @@ void opal_datatype_cuda_init(void)
         cuda_device[i].buffer_used.nb_elements = 0;
     }
     
-    cudaMalloc((void **)&cuda_desc_d, sizeof(ddt_cuda_desc_t));
-    cudaMallocHost((void **)&cuda_desc_h, sizeof(ddt_cuda_desc_t));
-    printf("size cuda_desc %d\n", sizeof(ddt_cuda_desc_t));
-    
-    // printf("malloc iov\n");
-    // for (i = 0; i < IOV_ARRAY_SIZE; i++) {
-    //     void* iov_base;
-    //     cudaMalloc( (void **)&iov_base, sizeof(char)*IOV_LEN);
-    //     cuda_desc_h->iov[i].iov_base = iov_base;
-    //     cuda_desc_h->iov[i].iov_len = IOV_LEN;
-    // }
-    
-    cudaMalloc((void **)(&ddt_cuda_pack_buffer), sizeof(char)*DT_CUDA_BUFFER_SIZE);
-    printf("malloc cuda packing buffer, %p\n", ddt_cuda_pack_buffer);
-    cudaMalloc((void **)(&ddt_cuda_unpack_buffer), sizeof(char)*DT_CUDA_BUFFER_SIZE);
-    printf("malloc cuda unpacking buffer, %p\n", ddt_cuda_unpack_buffer);
-
-    cuda_desc_h->iov[0].iov_base = ddt_cuda_pack_buffer;
-    cuda_desc_h->iov[0].iov_len = DT_CUDA_BUFFER_SIZE;
-    
-    cudaMalloc((void **)(&pBaseBuf_GPU), sizeof(char)*DT_CUDA_BUFFER_SIZE);
-    gpu_src_const = pBaseBuf_GPU;
-    gpu_dest_const = (unsigned char*)cuda_desc_h->iov[0].iov_base; 
-    
-    cuda_desc_h->description_max_count = 0;
-    cuda_desc_h->description_count = 0;
-    
     /* init cuda stream */
     cuda_streams = (ddt_cuda_stream_t*)malloc(sizeof(ddt_cuda_stream_t));
     for (i = 0; i < NB_STREAMS; i++) {
@@ -278,16 +220,10 @@ void opal_datatype_cuda_init(void)
     /* init cuda_iov */
     cuda_iov_count = CUDA_NB_IOV;
     
-    /* init description dist array */
-    cudaMalloc((void **)(&description_dist_d), sizeof(ddt_cuda_description_dist_t)*CUDA_MAX_NB_BLOCKS);
-    cuda_desc_h->description_dist = description_dist_d;
-    
     /* only for iov version */
     for (i = 0; i < NB_STREAMS; i++) {
         cudaMalloc((void **)(&cuda_iov_dist_d[i]), sizeof(ddt_cuda_iov_dist_t)*CUDA_MAX_NB_BLOCKS);
     }
-    
-    opal_datatype_cuda_debug = 1;
     
     // /* init size for double, float, char */
     // ALIGNMENT_DOUBLE = sizeof(double);
@@ -300,29 +236,6 @@ void opal_datatype_cuda_init(void)
 void opal_datatype_cuda_fini(void)
 {
     uint32_t i;
-    
-    if (cuda_desc_d != NULL) {
-        cudaFree(cuda_desc_d);
-        cuda_desc_d = NULL;
-    }
-    if (cuda_desc_h->description != NULL) {
-        cudaFree(cuda_desc_h->description);
-        cuda_desc_h->description = NULL;
-    }
-    if (cuda_desc_h->description_dist != NULL) {
-        cudaFree(cuda_desc_h->description_dist);
-        cuda_desc_h->description_dist = NULL;
-    }
-    printf("free iov\n");
-    if (cuda_desc_h != NULL) {    
-        for (i = 0; i < IOV_ARRAY_SIZE; i++) {
-            cudaFree(cuda_desc_h->iov[i].iov_base);
-            cuda_desc_h->iov[i].iov_base = NULL;
-        }
-    
-        cudaFreeHost(cuda_desc_h);
-        cuda_desc_h = NULL;
-    }
     
     /* destory cuda stream */
     for (i = 0; i < NB_STREAMS; i++) {
@@ -339,8 +252,6 @@ void opal_datatype_cuda_fini(void)
 void opal_cuda_sync_device(void)
 {
     cudaDeviceSynchronize();
-    pBaseBuf_GPU = gpu_src_const;
-    cuda_desc_h->iov[0].iov_base = (void*)gpu_dest_const;
 }
 
 int32_t opal_cuda_is_gpu_buffer(const void *ptr)
@@ -357,15 +268,6 @@ int32_t opal_cuda_is_gpu_buffer(const void *ptr)
     }
     /* Anything but CU_MEMORYTYPE_DEVICE is not a GPU memory */
     return (memType == CU_MEMORYTYPE_DEVICE) ? 1 : 0;
-}
-
-unsigned char* opal_cuda_get_gpu_pack_buffer()
-{
-    if (ddt_cuda_pack_buffer != NULL) {
-        return ddt_cuda_pack_buffer;
-    } else {
-        return NULL;
-    }
 }
 
 void* opal_cuda_malloc_gpu_buffer(size_t size, int gpu_id)
@@ -408,7 +310,7 @@ void* opal_cuda_malloc_gpu_buffer(size_t size, int gpu_id)
         cuda_list_push_head(&device->buffer_used, p);
         device->buffer_used_size += size;
         device->buffer_free_size -= size;
-        DT_CUDA_DEBUG( opal_cuda_output( 0, "Malloc GPU buffer %p, dev_id %d.\n", addr, dev_id); );
+        DT_CUDA_DEBUG( opal_cuda_output( 2, "Malloc GPU buffer %p, dev_id %d.\n", addr, dev_id); );
         return addr;
     }
 }
@@ -448,28 +350,16 @@ void opal_cuda_free_gpu_buffer(void *addr, int gpu_id)
     cuda_list_item_merge_by_addr(&device->buffer_free, ptr);
     device->buffer_free_size += size;
     device->buffer_used_size -= size;
-    DT_CUDA_DEBUG( opal_cuda_output( 0, "Free GPU buffer %p.\n", addr); );
+    DT_CUDA_DEBUG( opal_cuda_output( 2, "Free GPU buffer %p.\n", addr); );
 }
 
 void opal_dump_cuda_list(ddt_cuda_list_t *list)
 {
     ddt_cuda_buffer_t *ptr = NULL;
     ptr = list->head;
-    DT_CUDA_DEBUG( opal_cuda_output( 0, "DUMP cuda list %p, nb_elements %d\n", list, list->nb_elements); );
+    DT_CUDA_DEBUG( opal_cuda_output( 2, "DUMP cuda list %p, nb_elements %d\n", list, list->nb_elements); );
     while (ptr != NULL) {
-        DT_CUDA_DEBUG( opal_cuda_output( 0, "\titem addr %p, size %ld.\n", ptr->gpu_addr, ptr->size); );
+        DT_CUDA_DEBUG( opal_cuda_output( 2, "\titem addr %p, size %ld.\n", ptr->gpu_addr, ptr->size); );
         ptr = ptr->next;
-    }
-}
-
-/* from internal.h*/
-void opal_cuda_output(int output_id, const char *format, ...)
-{
-    if (output_id >= 0 && output_id <= OPAL_DATATYPE_CUDA_DEBUG_LEVEL) {
-        va_list arglist;
-        fprintf( stderr, "[Debug %d]: ", output_id );
-        va_start(arglist, format);
-        vfprintf(stderr, format, arglist);
-        va_end(arglist);
     }
 }
