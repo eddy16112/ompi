b6d56ebb85f9075be65806ea0bae651f48ebe9a2
Wei Wu <eddy16112@gmail.com>
2016-02-26 13:41:42 -0800

    new vector kernel

--- test/datatype/ddt_benchmark.c
+++ test/datatype/ddt_benchmark.c
@@ -1276,7 +1276,7 @@ int main( int argc, char* argv[] )
         printf("----matrix size %d-----\n", mat_size);
         if( outputFlags & CHECK_PACK_UNPACK ) {
             for (i = 1; i <= 5; i++) {
-                 local_copy_with_convertor(pdt, 1, 200000000, mat_size);
+               //  local_copy_with_convertor(pdt, 1, 200000000, mat_size);
             }
         }
         OBJ_RELEASE( pdt ); assert( pdt == NULL );
@@ -1339,13 +1339,13 @@ int main( int argc, char* argv[] )
     }
     
     
-    for (blk_len = 4000; blk_len <= 4000; blk_len += 2000) {
+    for (blk_len = 1000; blk_len <= 4000; blk_len += 2000) {
         printf( ">>--------------------------------------------<<\n" );
         printf( "Vector data-type (1024 times %d double stride 512)\n", blk_len );
         pdt = create_vector_type( MPI_DOUBLE, blk_len, blk_len, blk_len*2);
         if( outputFlags & CHECK_PACK_UNPACK ) {
             for (i = 0; i < 4; i++) {
-    //             vector_ddt( pdt, 1, pdt, 1, 1024*1024*200 , blk_len, blk_len, blk_len*2);
+                 vector_ddt( pdt, 1, pdt, 1, 1024*1024*200 , blk_len, blk_len, blk_len*2);
      //          vector_ddt_2d( pdt, 1, pdt, 1, 1024*1024*100 , 8192, blk_len, blk_len+128);
             }
         }
